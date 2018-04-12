330cac2fc4d8582f12dbcec0bd4922e9bce46c43
Michael Horn <michaelhorn1@hotmail.com>
2018-04-12 13:46:57 -0400

    more setup, added html pages

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+// Turn on error reporting
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+
+require_once ('vendor/autoload.php');
+session_start();
+require("model/db-functions.php");
+
+$f3 = Base::instance();
+
+//Set debug level
+$f3->set('DEBUG', 3);
+
+//Connect to the database
+$dbh = connect();
+
+//Login page
+$f3 -> route('GET|POST /', function($f3) {
+
+}
