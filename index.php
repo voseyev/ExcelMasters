8aced4ea96f96811b25d5e35c2fca30d357e8081
Michael Horn <michaelhorn1@hotmail.com>
2018-04-12 22:22:10 -0400

    Added some basic elements to the html pages

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
 
-// Turn on error reporting
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
-
-
 require_once ('vendor/autoload.php');
-session_start();
-require("model/db-functions.php");
 
 $f3 = Base::instance();
 
 //Set debug level
 $f3->set('DEBUG', 3);
 
-//Connect to the database
-$dbh = connect();
-
 //Login page
 $f3 -> route('GET|POST /', function($f3) {
+    if(isset($_POST['submit']))
+    {
+        header("Location: reports");
+    }
 
-}
+    $template = new Template();
+    echo $template->render('views/login.html');
+});
+
+$f3-> route('GET|POST /reports', function($f3) {
+
+    $template = new Template();
+    echo $template->render('views/reports.html');
+});
+
+//Run Fat-Free Framework
+$f3->run();
