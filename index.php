0a862c07543df02654e9a8bc717218437b508092
Vlad Oseyev <vladoseyev3@gmail.com>
2018-05-24 14:28:48 -0400

    Cleaned reports area

--- index.php
+++ index.php
@@ -49,9 +49,6 @@ $f3 -> route('GET|POST /', function($f3) {
 
 $f3-> route('GET|POST /reports', function($f3) {
     require("model/report.php");
-    $startDate = $_POST['startDate'];
-    $endDate = $_POST['endDate'];
-    selectData($startDate, $endDate);
 
 });
 
