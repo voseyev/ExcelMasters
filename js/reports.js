244b22b3ead48e0682b5501e6a5379a743dd0d7a
Tony <tony.g.thompson@gmail.com>
2018-05-24 19:22:41 -0700

    cleaning up report page some

---
+++ js/reports.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#datatable').DataTable();
+});
+
+function myFunction() {
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+    document.getElementById("showDate").innerHTML = startDate;
+    document.getElementById("showDate2").innerHTML = endDate;
+}
