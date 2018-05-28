4629ed70a723ae959c2787354e055379425e3ef3
Tony <tony.g.thompson@gmail.com>
2018-05-28 09:09:19 -0700

    fixed default date loading with summary info

--- model/report.php
+++ model/report.php
@@ -1,9 +1,11 @@
 <?php
+//login check
 if(empty($_SESSION['username']))
 {
     header("Location:inventory_management/ExcelMasters/");
 }
 
+//submit for file upload
 if(isset($_POST['submit']))
 {
     $fileName = $_FILES['file']['name'];
@@ -26,86 +28,82 @@ if(isset($_POST['submit']))
     }
 }
 
-//$currentDate = strftime('%F');
-echo "Todays date (revised to march)"  . " ";
-$currentDate = '2018-03-10' . "//////";
-echo $currentDate . "     ";
 
 
-$currentDate = explode('-', $currentDate);
-$year = $currentDate[0];
-$month   = $currentDate[1];
-$day  = $currentDate[2];
-
-$day = '01';
-
-$firstDayOfMonth = $year . "-" . $month . "-" . $day;
-echo " " . "First day of current days month";
-echo " " . $firstDayOfMonth;
-
-
-selectData($currentDate, $firstDayOfMonth);
-
-/*Date Picker Start and end Date*/
+/* Setting date range for select data*/
 if(isset($_POST['startDate']) && isset($_POST['endDate'])) {
+    echo "post set";
     $startDate = $_POST['startDate'];
     $endDate = $_POST['endDate'];
-    $result = selectData($startDate, $endDate);
-
-    //get data to display from database
-    $reports = getReports();
-    $f3->set('reports',$reports);
-    $f3->set('data', $result);
-
-
-    $numRows = 0;
-    $numCost = 0;
-    $totalWin = 0;
-    $totalCons = 0;
-    $totalCost = 0;
-    $totalMargin = 0;
-    foreach ($result as $row => $item) {
-        //returned columns
-        //item_num, title, end_date, win, pristine, cosignor, cost
-        //print_r($item);
-        $numRows++;
-        $totalWin += $item['win'];
-        $totalCons += $item['cosignor'];
-        if ($item['cost'] != null) {
-            $totalCost += $item['cost'];
-            $numCost++;
-        }
-        if ($item['profit'] != null) $totalMargin += $item['profit'];
-    }
-    if ($numCost != 0) {
-        $avgWin = $totalWin / $numRows;
-        $avgCons = $totalCons / $numRows;
-        $avgCost = $totalCost / $numCost;
-        $avgMargin = $totalMargin / $numCost;
-    } else if ($numRows != 0) {
-        $avgWin = $totalWin / $numRows;
-        $avgCons = $totalCons / $numRows;
-        $avgCost = 0;
-        $avgMargin = 0;
-    } else {
-        $avgWin = 0;
-        $avgCons = 0;
-        $avgCost = 0;
-        $avgMargin = 0;
-    }
-    $f3->set('startDate', $startDate);
-    $f3->set('endDate', $endDate);
-    $f3->set('totalItems', $numRows);
-    $f3->set('totalNoCost', $numRows - $numCost);
-    $f3->set('totalWin', $totalWin);
-    $f3->set('totalCons', $totalCons);
-    $f3->set('totalCost', $totalCost);
-    $f3->set('totalMargin', $totalMargin);
-    $f3->set('avgWin', $avgWin);
-    $f3->set('avgCons', $avgCons);
-    $f3->set('avgCost', $avgCost);
-    $f3->set('avgMargin', $avgMargin);
+} else {  //if search not sent use default date
+    //$currentDate = strftime('%F');
+    //TODO change back to dynamic current date
+    $endDate = '2018-03-10' . "//////";
+    $startDate = explode('-', $endDate);
+    $year = $startDate[0];
+    $month = $startDate[1];
+    $day = '01';
+    $startDate = $year . "-" . $month . "-" . $day;
+    echo "Tony: " . $startDate . "xxx" . $endDate;
 }
+//get item data
+$result = selectData($startDate, $endDate);
+//get file data to display from database
+$reports = getReports();
+$f3->set('reports',$reports);
+$f3->set('data', $result);
+
+//calculate summary info
+$numRows = 0;
+$numCost = 0;
+$totalWin = 0;
+$totalCons = 0;
+$totalCost = 0;
+$totalMargin = 0;
+foreach ($result as $row => $item) {
+    //returned columns:
+    //item_num, title, end_date, win, pristine, cosignor, cost
+    //print_r($item);
+    $numRows++;
+    $totalWin += $item['win'];
+    $totalCons += $item['cosignor'];
+    if ($item['cost'] != null) {
+        $totalCost += $item['cost'];
+        $numCost++;
+    }
+    if ($item['profit'] != null) $totalMargin += $item['profit'];
+}
+
+//checking which values are set to prevent dividing by 0
+if ($numCost != 0) {
+    $avgWin = $totalWin / $numRows;
+    $avgCons = $totalCons / $numRows;
+    $avgCost = $totalCost / $numCost;
+    $avgMargin = $totalMargin / $numCost;
+} else if ($numRows != 0) {
+    $avgWin = $totalWin / $numRows;
+    $avgCons = $totalCons / $numRows;
+    $avgCost = 0;
+    $avgMargin = 0;
+} else {
+    $avgWin = 0;
+    $avgCons = 0;
+    $avgCost = 0;
+    $avgMargin = 0;
+}
+
+$f3->set('startDate', $startDate);
+$f3->set('endDate', $endDate);
+$f3->set('totalItems', $numRows);
+$f3->set('totalNoCost', $numRows - $numCost);
+$f3->set('totalWin', $totalWin);
+$f3->set('totalCons', $totalCons);
+$f3->set('totalCost', $totalCost);
+$f3->set('totalMargin', $totalMargin);
+$f3->set('avgWin', $avgWin);
+$f3->set('avgCons', $avgCons);
+$f3->set('avgCost', $avgCost);
+$f3->set('avgMargin', $avgMargin);
 
 $template = new Template();
 echo $template->render('views/reports.html');
