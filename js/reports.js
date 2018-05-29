57edbeb219a67345177c8041f82f66fe7466d37e
Tony <tony.g.thompson@gmail.com>
2018-05-28 21:57:04 -0700

    removed testing date printing

--- js/reports.js
+++ js/reports.js
@@ -2,12 +2,6 @@ $(document).ready(function () {
     $('#datatable').DataTable();
 });
 
-function myFunction() {
-    var startDate = document.getElementById("startDate").value;
-    var endDate = document.getElementById("endDate").value;
-    document.getElementById("showDate").innerHTML = startDate;
-    document.getElementById("showDate2").innerHTML = endDate;
-}
 
 /*
 
