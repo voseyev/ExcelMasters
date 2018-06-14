39fa5556f4ec822f6c8bd64f8a83e7cc09a6941c
Michael Horn <michaelhorn1@hotmail.com>
2018-06-14 08:02:23 -0700

    admin now uploads correctly

--- index.php
+++ index.php
@@ -93,6 +93,30 @@ $f3-> route('GET|POST /edit', function($f3) {
         header("Location:inventory_management/ExcelMasters/");
     }
 
+    //submit for file upload
+    if(isset($_POST['submit']))
+    {
+        $fileName = $_FILES['file']['name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileType = $_FILES['file']['type'];
+        $fileData = $_FILES['file']['tmp_name'];
+
+        $lastId = uploadFile($fileName, $fileSize, $fileType, $fileData);
+
+        //Opens file contents into object
+        $fp = fopen($_FILES['file']['tmp_name'],'rb');
+        while(($line = fgets($fp)) !== false)
+        {
+            $newWord = "";
+            $arr = explode(',',$line);
+            if(is_numeric($arr[0]))
+            {
+                sendData($arr[0],$arr[2],$arr[3],$arr[4],$arr[5],$arr[6], $lastId);
+            }
+        }
+    }
+
+
     $data = selectQuantity2();
     foreach($data as &$arr)
     {
